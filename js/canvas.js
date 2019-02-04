a6888f8c8f3ab9c920d23648951ffcb81e596742
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro.local>
2019-02-04 15:05:26 -0600

    11)Still working on shooting functionality. Have added the trigger to pull in the mouse-click coordinates : Will need to finalize the event listener.

--- js/canvas.js
+++ js/canvas.js
@@ -10,6 +10,16 @@ let c = canvas.getContext('2d');
 var shipImg = new Image();
 shipImg.src ="Images/Single Ship Sprite.png";
 
+var shipDimensions = {
+    x: 60,
+    y: 50
+};
+
+var shipSpeed = {
+    dx: 2,
+    dy: 2
+}
+
 //Random Spaceships Generator 
 // shipImg.onload = function() {
 //     for (var i = 0; i < 2; i++) {
@@ -35,17 +45,17 @@ function Ship(x , y, dx, dy) {
     this.dx = dx;
     this.dy = dy;
     
-
     this.draw = function() {
         var shipImg = new Image();
         shipImg.src ="Images/Single Ship Sprite.png"; 
-        c.drawImage(shipImg, this.x, this.y, 60, 50);
+        c.drawImage(shipImg, this.x, this.y, shipDimensions.x, shipDimensions.y);
        
     }
 
     this.update = function() {
         if (this.x >= innerWidth - 58 || this.x < 0) {
             this.dx = -this.dx;
+            
         }
     
         if (this.y >= 400 || this.y < 0) {
@@ -59,15 +69,88 @@ function Ship(x , y, dx, dy) {
         this.draw();
 
         //Interactivity
-        if(mouse.x - this.x < 100 && mouse.x - this.x > -100
-            && mouse.y - this.y < 100 && mouse.y - this.y > -100) {
-            console.log("You hit a spaceship!")
-        }
+        // if(mouse.x - this.x < 50 && mouse.x - this.x > -50
+        //     && mouse.y - this.y < 100 && mouse.y - this.y > -100) {
+        //     this.x += 20;
+        // }
+
+        this.draw();
     }
 }
 
 var shipArray = [];
 
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+//Shoot Functionality 
+
+function isIntersect(mouse, shipImg) {
+    return Math.sqrt((mouse.x - shipImg.x) ** 2 + (mouse.y - shipImg.y) ** 2);
+  }
+  
+  canvas.addEventListener('click', function(e) {
+    const pos = {
+      x: e.clientX,
+      y: e.clientY
+    };
+    console.log(e.clientX, e.clientY);
+    shipArray.forEach(shipImg => {
+      if (isIntersect(mouse, shipImg)) {
+        console.log("a ship has been clicked");
+      }
+    });
+  });
+
+// shipImg.isHitBy = function(x, y) {
+//     return (x >= this.x && x <= this.x + shipDimensions.x && Y >= this.y && Y <= this. Y + shipDimensions.y)
+// }
+
+// canvas.addEventListener('click', function(e) {
+//     var canvasBounds = canvas.getBoundingClientRect();
+//     var clickX = e.pageX - canvasBounds.left;
+//     var clickY = e.pageY - canvasBounds.top;
+
+//     if(shipImg.isHitBy(clickX, clickY)) {
+//         console.log("A ship was hit");
+//     }
+// })
+
+// canvas.addEventListener('click', function(e) {
+//     var ship = new Ship()
+//         if (mouse.x || mouse.y) { 
+//            var x = e.clientX,
+//                y = e.clientY
+//           if(Math.pow(x - shipDimensions.x, 2) + Math.pow(y - shipDimensions.y, 2) < Math.pow(shipDimensions.x, 2))
+            
+//         }
+//         else { 
+//           x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+//           y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+//           console.log(x, y)
+//         } 
+//         x -= canvas.offsetLeft;
+//         y -= canvas.offsetTop;
+
+//         mouse.x = event.x;
+//         mouse.y = event.y;
+//         // if(mouse.x - this.x < 50 && mouse.x - this.x > -50
+//         //     && mouse.y - this.y < 100 && mouse.y - this.y > -100) {
+//         //     console.log("You have shot a ship!");
+//         // }
+// })
+
+//Reset Button
+document.querySelector('.reset-btn').addEventListener("click", init);
+
+
+/*----- functions -----*/
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
@@ -82,34 +165,16 @@ animate();
 
 // var shipArray = [];
 
-//Can adjust the veloticy and # of ships here//
+//Can adjust the velocity and # of ships here//
 function init() {
     shipArray = [];
     for (var i = 0; i < 10; i++) {
         var x = Math.random() * (innerWidth - 58);
         var y = Math.floor(Math.random() * 401);
-        var dx = (Math.random() - 0.5) * 3;
-        var dy = (Math.random() - 0.5) * 3;
+        var dx = (Math.random() - 0.5) * shipSpeed.dx;
+        var dy = (Math.random() - 0.5) * shipSpeed.dy;
         shipArray.push(new Ship(x, y, dx, dy));
     }
 }
 
 init();
-/*----- app's state (variables) -----*/
-
-
-/*----- cached element references -----*/
-
-
-/*----- event listeners -----*/
-
-//Shoot Functionality 
-window.addEventListener('click', 
-    function(event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
-})
-
-//Reset Button
-document.querySelector('.reset-btn').addEventListener("click", init);
-/*----- functions -----*/
