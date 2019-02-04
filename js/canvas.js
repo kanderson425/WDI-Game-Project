7a1f49acda637172ce18ca6c37b89aee2deb9858
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro.local>
2019-02-04 09:41:05 -0600

    8) Still working on README File. Fixed the header fonts and overlayed the spaceships on the background image : Working on adding audio filea and click event

--- js/canvas.js
+++ js/canvas.js
@@ -8,54 +8,139 @@ let c = canvas.getContext('2d');
 
 
 //Background Image
-var bgImg = new Image();
-bgImg.onload = function () {
-    c.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
-}
-
-bgImg.src = "Images/Background.png";
+// var bgImg = new Image();
+// bgImg.onload = function () {
+//     c.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+// }
+// bgImg.src = "Images/Background.png";
 
 //Alien Spaceship Image
-
 var shipImg = new Image();
 shipImg.src ="Images/Single Ship Sprite.png";
 
-function Ship(x, y, dx, dy) {
+//Random Spaceships Generator 
+// shipImg.onload = function() {
+//     for (var i = 0; i < 2; i++) {
+//         var x = Math.random() * window.innerWidth;
+//         var y = Math.random() * window.innerHeight;
+//         c.drawImage(shipImg, x, y, 50, 50);
+//     };
+// } 
+
+//Random moving spaceships
+
+// var x = Math.random() * innerWidth;
+// var y = Math.floor(Math.random() * 401);
+// var dx = (Math.random() -0.5) * 8;
+// var dy = (Math.random() - 0.5) * 8;
+
+window.addEventListener('click', 
+    function(event) {
+        mouse.x = event.x;
+
+})
+
+function Ship(x , y, dx, dy) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
-}
+    
+
     this.draw = function() {
-        shipImg.onload = function() {
-        c.drawImage(shipImg, 500, 100, 50, 50);
-        }  
-    }  
- 
+        var shipImg = new Image();
+        shipImg.src ="Images/Single Ship Sprite.png"; 
+        c.drawImage(shipImg, this.x, this.y, 60, 60);
+       
+    }
+
+    this.update = function() {
+        if (this.x >= innerWidth - 50 || this.x < 0) {
+            this.dx = -this.dx;
+        }
+    
+        if (this.y >= 400 || this.y < 0) {
+            this.dy = -this.dy;
+        }
+    
+    
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+
+
+}
+
 var shipArray = [];
 
-funtion init() {
-    shipArray = [];
-    for (var i = 0; i < 10; i++) {
-        var x = Math.random();
-        var y = Math.random();
-        var dx = (Math.random() - 0.5) * 2;
-        var dy = (Math.random() - 0.5) * 2;
-        shipArray.push(new Ship(x, y, dx, dy));
-    }
+//Can adjust the veloticy and # of ships here//
+for (var i = 0; i < 5; i++) {
+    shipArray.push(new Ship(x, y, dx, dy));
+    var x = Math.random() * (innerWidth - 50);
+    var y = Math.floor(Math.random() * 401);
+    var dx = (Math.random() - 0.5) * 10;
+    var dy = (Math.random() - 0.5) * 10;
 }
 
-function animate () {
+
+
+function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
 
+
     for (var i = 0; i < shipArray.length; i++) {
         shipArray[i].update();
-    };
+    }
 }
 
-init();
 animate();
+
+
+
+
+    
+//   this.update = function() {
+//       if(this.x > innerWidth || this.x - this.radius < 0) {
+//           this.dx = -this.dx;
+//       }
+
+//       if(this.y > innerHeight || this.y < 0) {
+//           this.dy = -this.dy;
+//       }
+//       this.x += this.dx;
+//       this.y += this.dy;
+
+//       this.draw();
+//   }  
+// }
+
+// var shipArray = [];
+
+// function init() {
+//     shipArray = [];
+//     for (var i = 0; i < 10; i++) {
+//         var x = Math.random();
+//         var y = Math.random();
+//         var dx = (Math.random() - 0.5) * 2;
+//         var dy = (Math.random() - 0.5) * 2;
+//         shipArray.push(new Ship(x, y, dx, dy));
+//     }
+// }
+
+// function animate () {
+//     requestAnimationFrame(animate);
+//     c.clearRect(0, 0, innerWidth, innerHeight);
+
+//     for (var i = 0; i < shipArray.length; i++) {
+//         shipArray[i].update();
+//     };
+// }
+
+// init();
+// animate();
 
 // /*Hillbilly Image with rifle
 // var hbImg = new Image();
@@ -76,112 +161,3 @@ animate();
 
 
 /*----- functions -----*/
-
-// var mouse = {
-//     x: undefined,
-//     y: undefined
-// }
-
-// var maxRadius = 40;
-// var minRadius = 2;
-
-// var colorArray = [
-//     '#2C3E50',
-//     '#E74C3C',
-//     '#ECF0F1',
-//     '#3498DB',
-//     '#3498DB'
-// ];   
-
-// window.addEventListener('mousemove', function(event) {
-//     mouse.x = event.x;
-//     mouse.y = event.y;
-// });
-
-// window.addEventListener('resize', function() {
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-
-//     init();
-// })
-
-
-// function Circle(x, y, dx, dy, radius) {
-//     this.x = x;
-//     this.y = y;
-//     this.dx = dx;
-//     this.dy = dy;
-//     this.radius = radius;
-//     this.minRadius = radius;
-//     this.color = c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)];
-
-//     this.draw = function() {
-//         c.beginPath();
-//         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-//         c.strokeStyle = "blue";
-//         c.stroke();
-//         c.fillStyle = this.color;
-//         c.fill();
-//     }
-    
-//     this.update = function() {
-//         if (this.x + this.radius  > innerWidth || this.x - this.radius < 0) {
-//             this.dx = -this.dx
-//         }
-    
-//         if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-//             this.dy = -this.dy;
-//         }
-//         this.x += this.dx;
-//         this.y += this.dy;
-
-//         //This is where the interactivity between circles and mouse occurs
-//         if(mouse.x - this.x < 50 && mouse.x - this.x > -50 && 
-//             mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-//             if(this.radius < maxRadius) {    
-//             this.radius += 1;
-//             }
-//         } else if (this.radius > this.minRadius) {
-//             this.radius -= 1;
-//         }
-//         this.draw();
-//     }
-// }
-
-// var circleArray = [];
-
-// for (var i = 0; i < 800; i++) {
-// var radius = Math.random() * 3 + 1;
-// var x = Math.random() * (innerWidth - radius * 2) + radius;
-// var y = Math.random() * (innerHeight - radius * 2) + radius;
-// var dx = (Math.random() - 0.5) * 2;
-// var dy = (Math.random() - 0.5) * 2;
-//     circleArray.push(new Circle(x, y, dx, dy, radius));
-// }
-
-// var circleArray = [];
-
-// function init() {
-//     circleArray = [];
-//     for (var i = 0; i < 2500; i++) {
-//     var radius = Math.random() * 3 + 1;
-//     var x = Math.random() * (innerWidth - radius * 2) + radius;
-//     var y = Math.random() * (innerHeight - radius * 2) + radius;
-//     var dx = (Math.random() - 0.5) * 2;
-//     var dy = (Math.random() - 0.5) * 2;
-//     circleArray.push(new Circle(x, y, dx, dy, radius));
-//     }
-// }
-
-
-// function animate() {
-//     requestAnimationFrame(animate);
-//     c.clearRect(0, 0, innerWidth, innerHeight);
-
-//     for (var i = 0; i < circleArray.length; i++) {
-//         circleArray[i].update();
-//     };
-
-// }
-// init();
-// animate();
