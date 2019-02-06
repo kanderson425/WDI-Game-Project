001f4f364b61128e53160376229c283551997661
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro.local>
2019-02-06 10:34:41 -0600

    19) Have made adjustments to the ship movements : Working on randomizing position/speed of multiple ships.

--- js/coin-tutorial.js
+++ js/coin-tutorial.js
@@ -1,17 +1,18 @@
 //width and height of canvas...
 var canvasWidth = window.innerWidth;
 var canvasHeight = window.innerHeight;
+var can = document.querySelector('canvas');;
+var context = can.getContext("2d");
 
 var bgImage = getBgImage();
 var shipImage = getShipImage();
 var shipsOnScreen = [];
-var risingSpeed = 0; //pixels per second...
-var shipSpeed = {
-    dx: 0,
-    dy: 0
-}
+var risingSpeed = 50; //pixels per second...
 
-// var shipSize = 200;
+var shipSpeed = {
+    dx: (Math.random() - 0.5) * 5,
+    dy: (Math.random() - 0.5) * 5
+}
 
 var shipDimensions = {
     width: .10 * canvasWidth,
@@ -19,7 +20,7 @@ var shipDimensions = {
 };
 
 var lastAnimationTime = 0;
-var howLongUntilNextShip = 1000;
+var howLongUntilNextShip = 10000;
 var nextShipOnScreen = 0;
 
 function doDraw() {
@@ -44,29 +45,54 @@ function doDraw() {
 
   //Now draw the ships
   if (lastAnimationTime != 0) {
-
     var deltaTime = new Date().getTime() - lastAnimationTime;
-    var shipRisePixels = Math.floor((deltaTime * risingSpeed) / 1000);
+    var shipVerticalPixels = Math.floor((deltaTime * shipSpeed.dx) / 1000);
+    var shipHorizontalPixels = Math.floor((deltaTime * shipSpeed.dx) / 1000);
+
 
     var survivingShips = [];
     for (var i = 0; i < shipsOnScreen.length; i++) {
-      var ship = shipsOnScreen[i];
-      ship.y = ship.y - shipRisePixels;
+        var ship = shipsOnScreen[i];
+        var newShip = {
+            x: newX,
+            y: newY
+          };    
+          function update() {
+            if (ship.x + shipDimensions.width - 30  > canvasWidth || ship.x < 0) {
+                shipSpeed.dx = -shipSpeed.dx
+            }
+            if (ship.y + shipDimensions.height > (canvasHeight * .60) || ship.y < 0) {
+                shipSpeed.dy = -shipSpeed.dy;
+            }
+                ship.x += shipSpeed.dx;
+                ship.y += shipSpeed.dy;
+            }    
       //the stl variable controlls the alpha of the image            
-      if (ship.y + 50 > 0) {
+      if (ship.y + 50 > 0 && ship.x + 50 > 0) {
         context.drawImage(shipImage, ship.x, ship.y);
         //this ship is still on the screen, so promote it to the new array...
         survivingShips.push(ship);
       }
+      update();
     }
     shipsOnScreen = survivingShips;
   }
+ 
   lastAnimationTime = new Date().getTime();
   //Wait, and then call this function again to animate:
   setTimeout(function() {
     doDraw();
   }, 30);
 }
+
+// function animate() {
+//     requestAnimationFrame(animate);
+//     context.clearRect(0,0, canvasWidth, canvasHeight);
+//     for (var i = 0; i < shipsOnScreen.length; i++) {
+//         let ship = shipsOnScreen[i];
+//         ship.update();
+//     };
+// }
 
 function setupClickHandler() {
   var can = document.querySelector('canvas');
@@ -80,7 +106,7 @@ function setupClickHandler() {
       //check to see if this coin has been clicked...
       if (x > ship.x && x < ship.x + shipDimensions.width && y > (ship.y + 60) && y < ship.y + (shipDimensions.height + 33)) {
         //ths ship will disappear because it is not inserted into the new array...
-        console.log("Mouse coordinates " + x + " " + y + " Ship coordinates " + x + " " + y);
+        console.log("Mouse coordinates " + x + " " + y + " Ship coordinates " + ship.x + " " + ship.y);
       } else {
         survivingShips.push(ship);
       }
@@ -90,10 +116,6 @@ function setupClickHandler() {
   };
 
 }
-
-
-doDraw();
-setupClickHandler();
 
 function getShipImage() {
   var shipImg = new Image(50, 50);
@@ -106,3 +128,23 @@ function getBgImage() {
     bgImg.src = "file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Images/Background.png";
     return bgImg;
 }
+
+document.querySelector('.reset-btn').addEventListener("click", init);
+
+function init() {
+    let shipsOnScreen = [];
+    let survivingShips = [];
+    let ship = shipsOnScreen[i];
+    for (var i = 0; i < 5; i++) {
+        var x = Math.random() * (canvasWidth - 58);
+        var y = Math.floor(Math.random() * (.7 * canvasHeight));
+        var dx = (Math.random() - 0.5) * shipSpeed.dx;
+        var dy = (Math.random() - 0.5) * shipSpeed.dy;
+        survivingShips.push(ship);
+    }
+}
+
+init();
+doDraw();
+// animate();
+setupClickHandler();
