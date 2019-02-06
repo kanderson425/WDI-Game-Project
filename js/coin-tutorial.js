a308307a682eed58939b9e7c52e25f31d3a7235e
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro.local>
2019-02-06 13:52:02 -0600

    20) Added sounds to my sounds file : integrating the clips into the game functionality. Still need to randomize the spaceship flight/movement.

--- js/coin-tutorial.js
+++ js/coin-tutorial.js
@@ -1,4 +1,26 @@
-//width and height of canvas...
+/*----- constants -----*/
+const sounds = {
+    gunshot:'file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Sounds/Gunshot1.mp3',
+    hillbillyYeehaw: 'file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Sounds/Hillbilly_Yeehaw.wav',
+    alien: 'file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Sounds/UFO_Ship.wav',
+    hillbillySong: 'file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Sounds/Bluegrass_Banjo_Song.wav'
+}
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+const player = new Audio();
+
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+function playSound(name) {
+    player.src = sounds[name];
+    player.play();
+}
+
 var canvasWidth = window.innerWidth;
 var canvasHeight = window.innerHeight;
 var can = document.querySelector('canvas');;
@@ -7,11 +29,15 @@ var context = can.getContext("2d");
 var bgImage = getBgImage();
 var shipImage = getShipImage();
 var shipsOnScreen = [];
-var risingSpeed = 50; //pixels per second...
+var lastAnimationTime = 0;
+var howLongUntilNextShip = 6000;
+var nextShipOnScreen = 0;
+
+document.querySelector('.reset-btn').addEventListener("click", init);
 
 var shipSpeed = {
-    dx: (Math.random() - 0.5) * 5,
-    dy: (Math.random() - 0.5) * 5
+    dx: (Math.random()) * 4,
+    dy: (Math.random()) * 4
 }
 
 var shipDimensions = {
@@ -19,9 +45,11 @@ var shipDimensions = {
     height: .10 * canvasHeight
 };
 
-var lastAnimationTime = 0;
-var howLongUntilNextShip = 10000;
-var nextShipOnScreen = 0;
+var ship = {
+    x: Math.floor(Math.random() * (canvasWidth)),
+    y: Math.floor(Math.random() * (.7 * canvasHeight)),
+}
+
 
 function doDraw() {
   var can = document.querySelector('canvas');;
@@ -31,8 +59,8 @@ function doDraw() {
 
   if (new Date().getTime() - nextShipOnScreen > 0) {
 
-    var newX = Math.floor(Math.random() * canvasWidth) + 1;
-    var newY = Math.floor(Math.random() * (.7 * canvasHeight));
+    var newX = Math.floor(Math.random() * (canvasWidth * .8));
+    var newY = Math.floor(Math.random() * (.55 * canvasHeight));
 
     var newShip = {
       x: newX,
@@ -46,33 +74,41 @@ function doDraw() {
   //Now draw the ships
   if (lastAnimationTime != 0) {
     var deltaTime = new Date().getTime() - lastAnimationTime;
-    var shipVerticalPixels = Math.floor((deltaTime * shipSpeed.dx) / 1000);
-    var shipHorizontalPixels = Math.floor((deltaTime * shipSpeed.dx) / 1000);
-
-
+    var shipVerticalPixels = Math.floor((deltaTime * shipSpeed.dx)/500);
+    var shipHorizontalPixels = Math.floor((deltaTime * shipSpeed.dx) /500);
     var survivingShips = [];
     for (var i = 0; i < shipsOnScreen.length; i++) {
         var ship = shipsOnScreen[i];
         var newShip = {
             x: newX,
             y: newY
-          };    
+          };
+        var newShipSpeed = {
+            dx: (Math.random() - 0.5) * 5,
+            dy: (Math.random() - 0.5) * 5,
+        }    
+
+        var newX = Math.floor(Math.random() * (canvasWidth * .6));
+        var newY = Math.floor(Math.random() * (.6 * canvasHeight));
+
           function update() {
             if (ship.x + shipDimensions.width - 30  > canvasWidth || ship.x < 0) {
                 shipSpeed.dx = -shipSpeed.dx
             }
-            if (ship.y + shipDimensions.height > (canvasHeight * .60) || ship.y < 0) {
+            if (ship.y > (canvasHeight * .60) || ship.y < 0) {
                 shipSpeed.dy = -shipSpeed.dy;
             }
                 ship.x += shipSpeed.dx;
                 ship.y += shipSpeed.dy;
             }    
       //the stl variable controlls the alpha of the image            
-      if (ship.y + 50 > 0 && ship.x + 50 > 0) {
+    //   if ((ship.y + shipDimensions.height < (canvasHeight * .6) && ship.y > 0) && 
+    //         (ship.x + shipDimensions.width - 30 < canvasWidth && ship.x > 0)) {
+    
         context.drawImage(shipImage, ship.x, ship.y);
         //this ship is still on the screen, so promote it to the new array...
         survivingShips.push(ship);
-      }
+    //   }
       update();
     }
     shipsOnScreen = survivingShips;
@@ -84,15 +120,6 @@ function doDraw() {
     doDraw();
   }, 30);
 }
-
-// function animate() {
-//     requestAnimationFrame(animate);
-//     context.clearRect(0,0, canvasWidth, canvasHeight);
-//     for (var i = 0; i < shipsOnScreen.length; i++) {
-//         let ship = shipsOnScreen[i];
-//         ship.update();
-//     };
-// }
 
 function setupClickHandler() {
   var can = document.querySelector('canvas');
@@ -128,23 +155,19 @@ function getBgImage() {
     bgImg.src = "file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Images/Background.png";
     return bgImg;
 }
-
-document.querySelector('.reset-btn').addEventListener("click", init);
-
 function init() {
     let shipsOnScreen = [];
     let survivingShips = [];
     let ship = shipsOnScreen[i];
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < shipsOnScreen.length; i++) {
         var x = Math.random() * (canvasWidth - 58);
         var y = Math.floor(Math.random() * (.7 * canvasHeight));
-        var dx = (Math.random() - 0.5) * shipSpeed.dx;
-        var dy = (Math.random() - 0.5) * shipSpeed.dy;
+        var dx = shipSpeed.x;
+        var dy = shipSpeed.dy;
         survivingShips.push(ship);
     }
 }
 
 init();
 doDraw();
-// animate();
 setupClickHandler();
