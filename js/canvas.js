375010665b42834bb7b50fe8d80fa2dbf4b49935
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro.local>
2019-02-07 10:26:46 -0600

    21) Making changes to the canvas window and have added sound effects : Need to add a start menu and end menu and improve on random ship movement logic.

--- js/canvas.js
+++ js/canvas.js
@@ -268,7 +268,7 @@ function changeImg() {
 //Can adjust the velocity and # of ships here//
 function init() {
     shipArray = [];
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 2; i++) {
         var x = Math.random() * (innerWidth - 58);
         var y = Math.floor(Math.random() * 401);
         var dx = (Math.random() - 0.5) * shipSpeed.dx;
--- js/coin-tutorial.js
+++ js/coin-tutorial.js
@@ -7,13 +7,23 @@ const sounds = {
 }
 
 /*----- app's state (variables) -----*/
+var shipSpeed = {
+    dx: (Math.random()) * 4,
+    dy: (Math.random()) * 4
+}
+var shipDimensions = {
+    width: .10 * canvasWidth,
+    height: .10 * canvasHeight
+};
 
+var score = 0;
 
 /*----- cached element references -----*/
 const player = new Audio();
 
 
 /*----- event listeners -----*/
+// document.querySelector('.reset-btn').addEventListener("click", init);
 
 /*----- functions -----*/
 function playSound(name) {
@@ -21,24 +31,18 @@ function playSound(name) {
     player.play();
 }
 
-var canvasWidth = window.innerWidth;
+var canvasWidth =  0.5 * window.innerWidth;
 var canvasHeight = window.innerHeight;
 var can = document.querySelector('canvas');;
 var context = can.getContext("2d");
 
-var bgImage = getBgImage();
+// var bgImage = getBgImage();
 var shipImage = getShipImage();
 var shipsOnScreen = [];
 var lastAnimationTime = 0;
-var howLongUntilNextShip = 6000;
+var howLongUntilNextShip = 3000;
 var nextShipOnScreen = 0;
-
-document.querySelector('.reset-btn').addEventListener("click", init);
-
-var shipSpeed = {
-    dx: (Math.random()) * 4,
-    dy: (Math.random()) * 4
-}
+var hillbillyImage = getHillbillyImage();
 
 var shipDimensions = {
     width: .10 * canvasWidth,
@@ -53,7 +57,7 @@ var ship = {
 
 function doDraw() {
   var can = document.querySelector('canvas');;
-  can.width  = window.innerWidth;
+  can.width  =window.innerWidth;
   can.height = window.innerHeight;
   var context = can.getContext("2d");
 
@@ -73,9 +77,9 @@ function doDraw() {
 
   //Now draw the ships
   if (lastAnimationTime != 0) {
-    var deltaTime = new Date().getTime() - lastAnimationTime;
-    var shipVerticalPixels = Math.floor((deltaTime * shipSpeed.dx)/500);
-    var shipHorizontalPixels = Math.floor((deltaTime * shipSpeed.dx) /500);
+    // var deltaTime = new Date().getTime() - lastAnimationTime;
+    // var shipVerticalPixels = Math.floor((deltaTime * shipSpeed.dx)/500);
+    // var shipHorizontalPixels = Math.floor((deltaTime * shipSpeed.dx) /500);
     var survivingShips = [];
     for (var i = 0; i < shipsOnScreen.length; i++) {
         var ship = shipsOnScreen[i];
@@ -84,8 +88,8 @@ function doDraw() {
             y: newY
           };
         var newShipSpeed = {
-            dx: (Math.random() - 0.5) * 5,
-            dy: (Math.random() - 0.5) * 5,
+            dx: (Math.random() - 0.5) * 7,
+            dy: (Math.random() - 0.5) * 7,
         }    
 
         var newX = Math.floor(Math.random() * (canvasWidth * .6));
@@ -98,14 +102,19 @@ function doDraw() {
             if (ship.y > (canvasHeight * .60) || ship.y < 0) {
                 shipSpeed.dy = -shipSpeed.dy;
             }
-                ship.x += shipSpeed.dx;
-                ship.y += shipSpeed.dy;
+                ship.x = ship.x + shipSpeed.dx;
+                ship.y = ship.y + shipSpeed.dy;
             }    
       //the stl variable controlls the alpha of the image            
     //   if ((ship.y + shipDimensions.height < (canvasHeight * .6) && ship.y > 0) && 
     //         (ship.x + shipDimensions.width - 30 < canvasWidth && ship.x > 0)) {
     
         context.drawImage(shipImage, ship.x, ship.y);
+        function drawScore() {
+            context.font = "20px Arial white";
+            context.fillText("Score: " + score, 15, 30);
+        }
+
         //this ship is still on the screen, so promote it to the new array...
         survivingShips.push(ship);
     //   }
@@ -125,15 +134,20 @@ function setupClickHandler() {
   var can = document.querySelector('canvas');
   //Here is the onclick handler
   can.onclick = function(e) {
-    var x = e.clientX;
-    var y = e.clientY;
+    var gunshot = new Audio('file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Sounds/Gunshot1.mp3');
+    gunshot.play();
+     x = e.clientX;
+     y = e.clientY;
     var survivingShips = [];
     for (var i = 0; i < shipsOnScreen.length; i++) {
       var ship = shipsOnScreen[i];
       //check to see if this coin has been clicked...
       if (x > ship.x && x < ship.x + shipDimensions.width && y > (ship.y + 60) && y < ship.y + (shipDimensions.height + 33)) {
         //ths ship will disappear because it is not inserted into the new array...
-        console.log("Mouse coordinates " + x + " " + y + " Ship coordinates " + ship.x + " " + ship.y);
+        score += 100;
+        console.log(score);
+        // console.log("Mouse coordinates " + x + " " + y + " Ship coordinates " + ship.x + " " + ship.y);
+        //  return score +100;
       } else {
         survivingShips.push(ship);
       }
@@ -150,24 +164,87 @@ function getShipImage() {
   return shipImg;
 }
 
-function getBgImage() {
-    var bgImg = new Image (window.innerWidth, window.innerHeight);
-    bgImg.src = "file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Images/Background.png";
-    return bgImg;
-}
-function init() {
-    let shipsOnScreen = [];
-    let survivingShips = [];
-    let ship = shipsOnScreen[i];
-    for (var i = 0; i < shipsOnScreen.length; i++) {
-        var x = Math.random() * (canvasWidth - 58);
-        var y = Math.floor(Math.random() * (.7 * canvasHeight));
-        var dx = shipSpeed.x;
-        var dy = shipSpeed.dy;
-        survivingShips.push(ship);
-    }
+function getHillbillyImage() {
+    var hillbillyImage = new Image (120, 150);
+    hillbillyImage.src = "Images/Single Ship Sprite.png";
+    return hillbillyImage;
 }
 
-init();
-doDraw();
+// function getBgImage() {
+//     var bgImg = new Image (window.innerWidth, window.innerHeight);
+//     bgImg.src = "file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Images/Background.png";
+//     return bgImg;
+// }
+
+function drawScore() {
+    context.font = "20px Arial white";
+    context.fillText("Score: " + score, 15, 30);
+}
+
+function drawTimer() {
+    var timeLeft = 30;
+    var downloadTimer = setInterval(function() {
+        document.getElementById("timer").innerHTML = timeLeft + " seconds remaining!";
+        timeLeft -= 1;
+        if(timeLeft <= 0) {
+            clearInterval(downloadTimer);
+            document.getElementById("timer").innerHTML = "Finished!"
+            endGame();
+        }
+    }, 1000);
+}    
+
+function startGame() {
+    var can = document.querySelector('canvas');;
+    can.width  = window.innerWidth;
+    can.height = window.innerHeight;
+    var context = can.getContext("2d");
+    context.drawImage(hillbillyImage, canvasWidth * .2, canvasHeight * .8);
+}
+
+function endGame() {
+    // alert(`You scored ${score} points!`);
+    context.drawImage(shipImage, ship.x, ship.y);
+    function drawScore() {
+        context.font = "20px Arial white";
+        context.fillText("Score: " + score, 15, 30);
+    }
+
+}
+
+function hillbillyYeehaw() {
+    var hillbillyYeehaw = new Audio('file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Sounds/Hillbilly_Yeehaw.wav');
+    setInterval(() => {
+        hillbillyYeehaw.play();
+    }, 15000)}
+
+ function alienSound() {
+     var alienSound = new Audio('file:///Users/kyleanderson/git/WDI/WDI-Game-Project/Sounds/UFO_Ship.wav');
+     setInterval(() => {
+         alienSound.play();
+     }, 10000)}  
+     
+function animate() {
+    requestAnimationFrame(animate);
+        drawScore();
+}     
+
+function init() {
+    var can = document.querySelector('canvas');;
+    can.width  = window.innerWidth;
+    can.height = window.innerHeight;
+    var context = can.getContext("2d");
+}
+
+
+// hillbillyYeehaw();
+// alienSound();
+startGame();
+endGame();
+drawTimer();
+getHillbillyImage();
+// init();
+doDraw(); 
 setupClickHandler();
+animate();
+
